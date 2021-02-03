268b55d494b209eea7c1dc68d0dd982b4b78bef8
Pavel Murygin <pavel.murygin@gmail.com>
2021-02-04 00:38:53 +0300

    Add initial structure for web-based client

---
+++ web-frontend/src/components/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './App.scss';
+
+const App = () => {
+    return <div>Hello world</div>;
+};
+
+export default App;
---
+++ web-frontend/src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from './components/App';
+import log from 'loglevel';
+
+// log.setLevel('trace');
+log.setLevel('error');
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root'),
+);
